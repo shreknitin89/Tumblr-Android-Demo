90a608696bf599287d965b8f8cb1247b551d1f54
Nitin Dasari <nitindasari95@gmail.com>
2019-11-19 23:06:13 -0800

    ui improvements

--- app/src/main/java/demo/nitin/tumblr_android_demo/base/PostViewHolder.kt
+++ app/src/main/java/demo/nitin/tumblr_android_demo/base/PostViewHolder.kt
@@ -3,15 +3,17 @@ package demo.nitin.tumblr_android_demo.base
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import demo.nitin.tumblr_android_demo.R
 import demo.nitin.tumblr_android_demo.utils.ImageLoadingUtil
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.post_layout.*
+import java.util.Locale
 
 class PostViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
     LayoutContainer {
     fun setPosts(uiPost: UiPost, fragment: Fragment) {
         post_title?.text = uiPost.blogName
-        tags?.text = uiPost.tags
+        tags?.text = String.format(Locale.US, fragment.getString(R.string.tags), uiPost.tags)
         ImageLoadingUtil.loadImage(fragment.requireContext(), uiPost.photo, post_image)
     }
 }
